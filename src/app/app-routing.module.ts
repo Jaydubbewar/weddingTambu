3e01cb64ab143bf69c13d9c2cad07a6f770c242d
JayDubbewar <jaydubbewar@gmail.com>
2023-02-22 20:09:58 +0530

    before vebdor submit form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogSignComponent } from './components/log-sign/log-sign.component';
+import { ShowDataComponent } from './components/show-data/show-data.component';
 
-const routes: Routes = [{ path: '', redirectTo: '', pathMatch: 'full' },
-{ path: 'login', component: LogSignComponent }];
+// const routes: Routes = [{ path: '', component: ShowDataComponent, pathMatch: 'full' },
+// { path: 'login', component: LogSignComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
